b98fc3d4d03b9f25e7bba28418019b9a6cbbad83
h23maarv <h23maarv@du.se>
2025-04-14 10:23:20 +0200

    La till primtal

--- main.py
+++ main.py
@@ -12,3 +12,14 @@ process.
 3. Write a Python function to check whether a given number n is prime or
 not using an efficient approach
 """
+
+def is_prime(n):
+    if n <= 1:
+        return False
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+print(is_prime(2))   # Output: True
+print(is_prime(4))  # Output: False
