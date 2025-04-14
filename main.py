c92431cc30cbb348ac6e3259f9b9f07781a24024
Isak Lagerberg <dev.lagerberg@gmail.com>
2025-04-14 10:16:01 +0200

    La till uppgiften

---
+++ main.py
@@ -0,0 +1,14 @@
+"""
+Lab 3
+1. Write a Python function to compute the Greatest Common Divisor
+(GCD) of two numbers using the Euclidean algorithm. Then, use this
+function to find the GCD of three numbers by calling the two-number
+GCD function recursively.
+2. Write a Python function to find the Least Common Multiple (LCM) of
+two numbers by starting from the maximum of the two and checking
+each multiple until one is divisible by both numbers. The function
+should return the smallest common multiple found through this
+process.
+3. Write a Python function to check whether a given number n is prime or
+not using an efficient approach
+"""
